9f042c691aade3d8aea4e2eaa9a8c1900f35c99d
Maxim Slipenko <max.slipenko@gmail.com>
2020-04-11 18:20:39 +0300

    Add gtag.ts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,53 +1,36 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import Router from 'next/router'
-
-function isLocal (host): boolean {
-  return location.hostname === host
-}
-
-function isDev (): boolean {
-  return process.env.NODE_ENV !== 'production'
-}
+import { shouldTrack } from '../lib/gtag'
 
 class MyDocument extends Document {
-  shoudTrack = true
-
   static async getInitialProps (ctx) {
     // const shouldNotTrack = !isLocal('localhost') && !isDev()
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
 
-  componentDidMount () {
-    let { gtag, dataLayer } = window
-    console.log('HERE!')
-    dataLayer = dataLayer || []
-    gtag = function (...args): void {
-      console.log(args)
-      dataLayer.push(args)
-    }
-    gtag('js', new Date())
-    gtag('config', process.env.GOOGLE_ANALYTICS)
-    Router.events.on('routeChangeComplete', url => {
-      setTimeout(() => {
-        console.log('test!')
-        gtag('config', process.env.GA_TRACKING_ID, {
-          page_location: url,
-          page_title: document.title
-        })
-      }, 0)
-    })
-  }
-
   render () {
     return (
       <Html>
         <Head>
-          {this.shoudTrack && <script
+          { shouldTrack && <><script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
-          />}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `
+            }}
+          /></>
+          }
         </Head>
         <body>
           <Main />
